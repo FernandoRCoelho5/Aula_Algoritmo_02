c46807ce42a8dc08509f161555379acb1d3bbd27
FernandoRCoelho5 <ramoscoelhofernando@gmail.com>
2025-09-22 19:21:05 -0300

    Modificação Código

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function printBoard() {
     console.log('\n');
 }
 function checkWin() {
-    // Check rows and columns
+    // Anáslise das linhas e colunas
     for (let i = 0; i < 3; i++) {
         if (board[i][0] === currentPlayer && board[i][1] === currentPlayer && board[i][2] === currentPlayer) return true;
         if (board[0][i] === currentPlayer && board[1][i] === currentPlayer && board[2][i] === currentPlayer) return true;
     }
-    // Check diagonals
+    // Análise das diagonais
     if (board[0][0] === currentPlayer && board[1][1] === currentPlayer && board[2][2] === currentPlayer) return true;
     if (board[0][2] === currentPlayer && board[1][1] === currentPlayer && board[2][0] === currentPlayer) return true;
     return false;
